7dd16bd5c84217f4e5bd8fe44b6a6ebe82cd46ae
ashisshh2105 <aashishballia2@gmail.com>
2026-09-14 11:24:33 +0530

    Solved: Min and Max in Array [Easy]

---
+++ GeeksForGeeks/Easy/Min and Max in Array/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public static ArrayList<Integer> getMinMax(int[] arr) {
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        result.add(min);
+        result.add(max);
+
+        return result;
+    }
+}
